e3cb252671e5e8d44e50cdcd47ceecdb7d14bb52
grantposell <grantposell@gmail.com>
2022-01-15 22:23:48 -0600

    Took away background picture to have more clarity

--- src/App.js
+++ src/App.js
@@ -8,14 +8,21 @@ class App extends Component {
   render() {
     return (
       <div style={{ height: '100vh', position: 'relative' }}>
-        <Layout style={{ background: `url(${NYC})`}}>
-          <Header transparent style={{ color: 'black' }}>
+        <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration:'none', color: 'white'}} to="/">Joe Woehling</Link>} scroll>
+            <Navigation>
+              <Link to="/">Home</Link>
+              <Link to="/portfolio">Portfolio</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          {/* <Header transparent style={{ color: 'black' }}>
             <Navigation>
               <Link style={{color: 'black', fontWeight: 'bold'}}to="/">Home</Link>
               <Link style={{color: 'black', fontWeight: 'bold'}}to="/portfolio">Portfolio</Link>
               <Link style={{color: 'black', fontWeight: 'bold'}}to="/contact">Contact</Link>
             </Navigation>
-          </Header>
+          </Header> */}
           <Content>
           <div className="page-content" />
           <Main></Main>
