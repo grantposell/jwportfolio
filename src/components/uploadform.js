fef33b470ce80f30a8a25774e92e3ee8f51e4ce4
grantposell <grantposell@gmail.com>
2022-01-20 18:03:48 -0600

    Fixed the blank page issue

--- src/components/uploadform.js
+++ src/components/uploadform.js
@@ -12,21 +12,23 @@ const UploadForm = () => {
 
     const changeHandler = (e) => {
         let selected = e.target.files[0];
+        
         if (selected && types.includes(selected.type)) {
             setFile(selected);
+            setError('')
         } else {
             setFile(null);
             setError('Please select an image file (png or jpeg)')
         }
-    }
+    };
 
     return (
         <form>
             <label>
                 <input type="file" onChange={changeHandler} />
-                <span>+</span>
             </label>
-            <div className='output'>{error && <div className="error">{error}</div>}
+            <div className='output'>
+                {error && <div className="error">{ error }</div>}
                 {file && <div>{file.name}</div>}
                 {file && <ProgressBar file ={file} setFile={setFile}/>}
             </div>
